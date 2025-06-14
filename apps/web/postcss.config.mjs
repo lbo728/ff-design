e3ac5c2fca739c9564e6173609a09e715a80a6d3
lbo728 <72309817+lbo728@users.noreply.github.com>
2025-06-15 00:09:27 +0900

    init base structure for monorepo design system

--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,1 +1,1 @@
-export { default } from "@workspace/ui/postcss.config";
+export { default } from "@ff-design/react/postcss.config";
---
+++ packages/react/eslint.config.js
@@ -0,0 +1,4 @@
+import { config } from "@ff-design/eslint-config/react-internal";
+
+/** @type {import("eslint").Linter.Config} */
+export default config;
